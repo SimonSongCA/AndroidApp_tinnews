bd3d4f7ef422bd9e760b5dd6e52467953c7d2de1
SimonSongCA <SimonSongCA@gmail.com>
2020-05-27 21:27:18 -0700

    20200527  Created the news API and Retrofit to parse news from the web on Android

---
+++ app/src/main/java/com/tinnews/TinNewsApplication.java
@@ -0,0 +1,13 @@
+package com.tinnews;
+
+import android.app.Application;
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+
+public class TinNewsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Gander.setGanderStorage(GanderIMDB.getInstance());
+    }
+}
